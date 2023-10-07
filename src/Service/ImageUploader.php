b78ff1912fe427fb53a914adcdd94ab842f8f071
Zouheir Mohamed <m.zouheir74@gmail.com>
2023-10-07 15:40:36 +0200

    created new ImageUploader service

---
+++ src/Service/ImageUploader.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class ImageUploader
+{
+    public function __construct(
+        private string $imageDirectory,
+        private SluggerInterface $slugger,
+    ) {
+    }
+
+    public function upload(UploadedFile $file): string
+    {
+        $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+        $safeFilename = $this->slugger->slug($originalFilename);
+        $fileName = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
+
+        try {
+            $file->move($this->getImageDirectory(), $fileName);
+        } catch (FileException $e) {
+            // ... handle exception if something happens during file upload
+        }
+
+        return $fileName;
+    }
+
+    public function getImageDirectory(): string
+    {
+        return $this->imageDirectory;
+    }
+}
