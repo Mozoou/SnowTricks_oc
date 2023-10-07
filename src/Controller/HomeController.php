ef6a791fb9027895d19d24abee63167b0da4acee
Zouheir Mohamed <m.zouheir74@gmail.com>
2023-10-07 15:41:19 +0200

    added list of tricks on home page (with pagination)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,35 @@
 
 namespace App\Controller;
 
+use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(TrickRepository $trickRepository, Request $request): Response
     {
-        return $this->render('home/index.html.twig');
+        $page = $request->query->getInt('page', 1);
+        $itemsPerPage = 15;
+        $offset = ($page - 1) * $itemsPerPage;
+
+        $queryBuilder = $trickRepository->createQueryBuilder('t');
+        $queryBuilder
+            ->setFirstResult($offset)
+            ->setMaxResults($itemsPerPage);
+
+        $tricks = $queryBuilder->getQuery()->getResult();
+
+        $totalItems = $trickRepository->createQueryBuilder('t')->select('COUNT(t.id)')->getQuery()->getSingleScalarResult();
+        $totalPages = ceil($totalItems / $itemsPerPage);
+
+        return $this->render('home/index.html.twig', [
+            'tricks' => $tricks,
+            'page' => $page,
+            'totalPages' => $totalPages,
+        ]);
     }
 }
